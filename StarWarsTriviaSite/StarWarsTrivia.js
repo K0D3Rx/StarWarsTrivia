597750810c0c48eb9bb543403d0fbbe682fdb57f
Enzo Gimenez <36288635+K0D3Rx@users.noreply.github.com>
2018-04-17 15:41:14 -0400

    Update StarWarsTrivia.js
    cleaned up code a bit.

--- StarWarsTriviaSite/StarWarsTrivia.js
+++ StarWarsTriviaSite/StarWarsTrivia.js
@@ -1,55 +1,45 @@
 //*--------------------------Star Wars Trivia----------------------------*\\
 
 var questionsAnswers = [
-	["What is Luke Skywalker's father's name?", "anakin"],
-	["Who shot first?", "han solo"],
-	["True or False, Leia is Rey's mother.", "false"],
-	["On what planet did Rey and Finn first board the millenium falcon?", "jakku"],
-	["Who is leader of the knights of ren?","kylo ren"],
-	["who is considered to be the most annoying Star Wars character of all time?", "jar jar binks"],
-	["True or false, Darth Vader has a higher medoclorian count than master yoda", "true"]
-]; 
+	{ question: "What is Luke Skywalker's father's name?", answer: "anakin"},
+	{ question: "Who shot first?", answer: "han solo"},
+	{ question: "True or False, Leia is Rey's mother.", answer: "false"},
+	{ question: "On what planet did Rey and Finn first board the millenium falcon?", answer: "jakku"},
+	{ question: "Who is leader of the knights of ren?", answer: "kylo ren"},
+	{ question: "who is considered to be the most annoying Star Wars character of all time?", answer: "jar jar binks"},
+	{ question: "True or false, Darth Vader has a higher medoclorian count than master yoda", answer: "true"}
+];   
 
 var answerRight = 0; 
-var answer;
+var answer;   
 var response;
 var question;    
 var html;  
-var wrong = []; 
-var correct = [];  
-
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
-} 
+var wrong = [];    
+var correct = [];        
  
-function buildList(quiz){
-	var listHTML = "<ol>";
-	for ( var j = 0; j < quiz.length; j += 1 ){
-		listHTML += "<li>" + quiz[j] + "</li>"; 
-	} 	listHTML += "</ol>"; 
-	  	return listHTML;
+function print(html) {
+  document.write(html); 
 } 
 
-for ( var i = 0; i < questionsAnswers.length; i += 1 ){
-	question = questionsAnswers[i][0];
-	answer = questionsAnswers[i][1];
+for ( var i = 0; i < questionsAnswers.length; i ++ ){
+	question = questionsAnswers[i].question;  
+	answer = questionsAnswers[i].answer; 
 	response = prompt(question);
 	response = response.toLowerCase();
 	if ( response === answer){
-		answerRight += 1;  
-		correct.push(question); 
-	} else{   
-		wrong.push(question);
-	}  
-}  
+		answerRight ++;  
+		correct.push(question);  
+	} else{    
+		wrong.push(question); 
+	} 
+}   
 
-html = "You got " + answerRight + " question(s) right.";  
-html += '<h2>You got these questions correct:</h2>';
-html += buildList(correct);
-html += '<h2>You got these questions wrong:</h2>';
-html += buildList(wrong); 
-print(html)  
+
+html += '<p> You got ' + answerRight + ' question(s) right.</p>'; 
+html += '<p> correct question(s): ' + correct + '</p>'; 
+html += '<p> Incorrect question(s): ' + wrong + '</p>';  
+print(html);     
 
 
 
